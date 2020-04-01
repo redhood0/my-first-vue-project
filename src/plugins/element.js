26e4bcbca63d570e4d0b25a3e3ec2d2dc0cd7f83
redhood <2043570808@qq.com>
2020-04-02 01:18:26 +0800

    true commit

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import 'element-ui/lib/theme-chalk/index.css'
-import {
-    Element,
-    Message
-} from 'element-ui';
+import Element from 'element-ui'
+import {Message} from 'element-ui'
 
 Vue.use(Element)
 Vue.prototype.$message = Message
